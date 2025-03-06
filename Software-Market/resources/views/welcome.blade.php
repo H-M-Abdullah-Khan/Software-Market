21229d7e668da160fe29aa5adbeef5fee825ec86
Hafiz Muhammad Abdullah Khan <m.abdullahkhan9990@gmail.com>
2025-03-07 02:15:39 +0500

    update

--- Software-Market/resources/views/welcome.blade.php
+++ Software-Market/resources/views/welcome.blade.php
@@ -42,10 +42,36 @@
                             <a
                                 href="{{ route('register') }}"
                                 class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                                Register
+                            Register
                             </a>
                         @endif
                     @endauth
+
+
+
+                    @auth('admin')
+                    <a
+                        href="{{ url('/admin/dashboard') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                    >
+                        Dashboard
+                    </a>
+                @else
+                    <a
+                        href="{{ route('admin.login') }}"
+                        class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] text-[#1b1b18] border border-transparent hover:border-[#19140035] dark:hover:border-[#3E3E3A] rounded-sm text-sm leading-normal"
+                    >
+                        Admin Log in
+                    </a>
+
+                    @if (Route::has('admin.register'))
+                        <a
+                            href="{{ route('admin.register') }}"
+                            class="inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                        Admin Register
+                        </a>
+                    @endif
+                @endauth
                 </nav>
             @endif
         </header>
