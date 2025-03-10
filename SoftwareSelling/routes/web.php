0556774646069b952d256a382abb489b84ac5cf0
Hafiz Muhammad Abdullah Khan <m.abdullahkhan9990@gmail.com>
2025-03-10 19:57:46 +0500

    update

--- SoftwareSelling/routes/web.php
+++ SoftwareSelling/routes/web.php
@@ -16,5 +16,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::post('/logout', [ProfileController::class, 'destroy'])->name('logout');
 
 require __DIR__.'/auth.php';
